137bc580044250ab8089f46802ddca486e2d62ec
949582502 <949582502@qq.com>
2021-01-10 18:59:40 +0800

    创建分支

--- HelloGit/src/main/java/com/qs/git/HelloGit.java
+++ HelloGit/src/main/java/com/qs/git/HelloGit.java
@@ -3,6 +3,7 @@ package com.qs.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit");
-        System.out.println("更新");
+        System.out.println("更新1");
+        System.out.println("创建分支");
     }
 }
